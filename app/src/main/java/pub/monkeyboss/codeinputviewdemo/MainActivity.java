920054d43f5cfd10221b37b727e79ec35bfd02f4
monkeyboss <blueyea@yeah.net>
2017-10-24 16:44:23 +0800

    update demo

--- app/src/main/java/pub/monkeyboss/codeinputviewdemo/MainActivity.java
+++ app/src/main/java/pub/monkeyboss/codeinputviewdemo/MainActivity.java
@@ -2,6 +2,9 @@ package pub.monkeyboss.codeinputviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import pub.monkeyboss.widget.CodeInputView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CodeInputView codeInputView = (CodeInputView) findViewById(R.id.codeInputView);
+        codeInputView.setOnInputListener(new CodeInputView.OnInputListener() {
+            @Override
+            public void onInput(int position, char c, String content) {
+
+            }
+
+            @Override
+            public void onDelete(String content) {
+
+            }
+
+            @Override
+            public void onComplete(String content) {
+
+            }
+        });
     }
 }
